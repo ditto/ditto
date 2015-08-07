65571c181893c76476372c2b491a10860e22d16a
Francisco Ferreira <fferre8@cs.mcgill.ca>
2015-08-06 22:10:56 -0700

    First version of capture avoiding substitution

--- src/Ditto/Sub.hs
+++ src/Ditto/Sub.hs
@@ -11,4 +11,17 @@ fv (a :@: b) = fv a ++ fv b
 
 
 sub :: (Name , Exp) -> Exp -> Exp
-sub = error "Subsitution not implemented"
+sub (x, a) (EVar y) | x == y = a
+sub (x, a) (EVar y) | x /= y = a
+sub (x, a) Type = Type
+sub (x, a) (Lam y _B b) | x == y = (Lam y (sub (x, a) _B) b)
+sub (x, a) (Lam y _B b) | x /= y && y `notElem` (fv a) =
+  Lam y (sub (x, a) _B) (sub (x, a) b)
+sub (x, a) (Lam y _B b) | x /= y && y `elem` (fv a) =
+  error "sub cannot rename variables if not in the monad"
+sub (x, a) (Pi y _A _B) | x == y = (Pi y (sub (x, a) _A) _B)
+sub (x, a) (Pi y _A _B) | x /= y && y `notElem` (fv a) =
+  Pi y (sub (x, a) _A) (sub (x, a) _B)
+sub (x, a) (Pi y _B b) | x /= y && y `elem` (fv a) =
+  error "sub cannot rename variables if not in the monad"
+sub (x, a) (f :@: b) = (sub (x, a) f) :@: (sub (x, a) b)
