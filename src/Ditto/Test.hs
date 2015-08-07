f88e6b9349366dd5ec93446867411a266be2ede4
larrytheliquid <larrytheliquid@gmail.com>
2015-08-07 12:11:52 -0700

    More parsing tests

--- src/Ditto/Test.hs
+++ src/Ditto/Test.hs
@@ -3,13 +3,21 @@ import Ditto.Syntax
 import Ditto.Parse
 import Test.HUnit
 
-testParse :: String -> Exp -> Test
-testParse s a = TestCase $ case parseE s of
+testParse :: String -> Maybe Exp -> Test
+testParse s ma = TestCase $ case parseE s of
   Left error -> assertFailure ("Parse error:\n" ++ show error)
-  Right a' -> a @=? a'
+  Right a -> maybe (return ()) (@=? a) ma
 
 unitTests :: Test
-unitTests = TestList [testParse "Type" Type]
+unitTests = TestList
+  [ testParse "Type" (Just Type)
+  , testParse "A" (Just (EVar "A"))
+  , testParse "F x y z" Nothing
+  , testParse "(x : A) (y : B) : Type" Nothing
+  , testParse "(x : A) (y : B x) : C x y" Nothing
+  , testParse "(x : A) (y : B) -> c" Nothing
+  , testParse "(x : A) (y : B x) : C (((z : A) -> z) x) (g x y)" Nothing
+  ]
 
 runTests :: IO Counts
 runTests = runTestTT unitTests
