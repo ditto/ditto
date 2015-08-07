56112f520a23c83d8ed164580f10ff491fdcaba8
larrytheliquid <larrytheliquid@gmail.com>
2015-08-07 15:38:21 -0700

    Get rid of coverage errors

--- src/Ditto/Sub.hs
+++ src/Ditto/Sub.hs
@@ -15,16 +15,16 @@ sub :: (Name , Exp) -> Exp -> Exp
 sub (x, a) (Form y is) = error "sub into form not implemented"
 sub (x, a) (Con y as) = error "sub into con not implemented"
 sub (x, a) (EVar y) | x == y = a
-sub (x, a) (EVar y) | x /= y = EVar y
+sub (x, a) (EVar y) = EVar y
 sub (x, a) Type = Type
 sub (x, a) (Lam y _B b) | x == y = (Lam y (sub (x, a) _B) b)
-sub (x, a) (Lam y _B b) | x /= y && y `notElem` (fv a) =
+sub (x, a) (Lam y _B b) | y `notElem` (fv a) =
   Lam y (sub (x, a) _B) (sub (x, a) b)
-sub (x, a) (Lam y _B b) | x /= y && y `elem` (fv a) =
+sub (x, a) (Lam y _B b) =
   error "sub cannot rename variables if not in the monad"
 sub (x, a) (Pi y _A _B) | x == y = (Pi y (sub (x, a) _A) _B)
-sub (x, a) (Pi y _A _B) | x /= y && y `notElem` (fv a) =
+sub (x, a) (Pi y _A _B) | y `notElem` (fv a) =
   Pi y (sub (x, a) _A) (sub (x, a) _B)
-sub (x, a) (Pi y _B b) | x /= y && y `elem` (fv a) =
+sub (x, a) (Pi y _B b) =
   error "sub cannot rename variables if not in the monad"
 sub (x, a) (f :@: b) = (sub (x, a) f) :@: (sub (x, a) b)
