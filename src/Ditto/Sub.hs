b05bf997e81af0696d2773febecc71979cc1500d
Francisco Ferreira <fferre8@cs.mcgill.ca>
2015-08-06 21:55:23 -0700

    free variables

--- src/Ditto/Sub.hs
+++ src/Ditto/Sub.hs
@@ -1,6 +1,14 @@
 module Ditto.Sub where
 import Ditto.Syntax
+import Data.List
+
+fv :: Exp -> [Name]
+fv (EVar x) = [x]
+fv Type = []
+fv (Pi n _A _B) = fv _A ++ (n `delete` (fv _B))
+fv (Lam n _A a) = fv _A ++ (n `delete` (fv a))
+fv (a :@: b) = fv a ++ fv b
+
 
 sub :: (Name , Exp) -> Exp -> Exp
 sub = error "Subsitution not implemented"
-
