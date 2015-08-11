dcc6f59524c225246cc6c4385114413c507f12f1
Francisco Ferreira <fferre8@cs.mcgill.ca>
2015-08-11 14:18:05 -0700

    Added eliminator test, and a small fix for name generation

--- src/Ditto/Monad.hs
+++ src/Ditto/Monad.hs
@@ -55,7 +55,7 @@ gensymHint x = do
   state@DittoS {nameId = nameId} <- get
   let nameId' = succ nameId
   put state { nameId = nameId' }
-  return $ "$" ++ show x ++ show nameId'
+  return $ "$" ++ x ++ show nameId'
 
 addSig :: Sigma -> TCM ()
 addSig s = do
@@ -96,7 +96,9 @@ addElim _X = do
   let (etel, res) = elimType _X (t, _P, _Is, _Cs')
   -- body of the eliminator
   let a = lams etel (Elim _X $ varNames etel)
-  -- add the delta equivalent term
+  -- add the delta equivalent term it would be nice to type-check
+  -- this, but the check module uses this one, so one can't call it
+  -- from here
   addDef ("elim" ++ fromPName _X) a (pis etel res)
 
 
