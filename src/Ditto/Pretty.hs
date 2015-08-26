1f8e73f444c170c02178e7f99fb199d564bcfa20
larrytheliquid <larrytheliquid@gmail.com>
2015-08-26 13:01:25 -0700

    Basic parentheses minimizing pretty printing.

--- src/Ditto/Pretty.hs
+++ src/Ditto/Pretty.hs
@@ -3,26 +3,46 @@ module Ditto.Pretty where
 import Ditto.Syntax
 import Text.PrettyPrint.Boxes
 
+----------------------------------------------------------------------
+
+ppExp :: Exp -> Box
+ppExp = ppwExp NoWrap
+
+ppBind :: Name -> Exp -> Box
+ppBind x _A = parens $ ppName x <+> oft <+> ppExp _A
+
+ppwExp :: Wrap -> Exp -> Box
+ppwExp w Type = text "Type"
+ppwExp w (Var x) = ppName x
+ppwExp w (Pi _A (Bind x _B)) = righty w $ ppBind x _A <+> oft <+> ppExp _B
+ppwExp w (Lam _A (Bind x a)) = righty w $ ppBind x _A <+> arr <+> ppExp a
+ppwExp w (Form _X _Is) = ppPName _X
+ppwExp w (Con _X as) = ppPName _X
+ppwExp w (Red _X as) = ppPName _X
+ppwExp w (f :@: a) = lefty w $ ppwExp NoWrapL f <+> ppwExp Wrap a
+
+----------------------------------------------------------------------
+
+data Wrap = Wrap | NoWrapL | NoWrap
+
+righty :: Wrap -> Box -> Box
+righty NoWrap = id
+righty _ = parens
+
+lefty :: Wrap -> Box -> Box
+lefty NoWrap = id
+lefty NoWrapL = id
+lefty Wrap = parens
+
+----------------------------------------------------------------------
+
 ppSig :: Sigma -> Box
 ppSig (Def x a _A) = (ppName x <+> oft <+> ppExp _A) // (ppName x <+> def <+> ppExp a)
 ppSig (DForm _X _Is) = ppPName _X <+> text "Data"
 ppSig (DCon _Y _As _X _Is) = ppPName _Y <+> text "constructor of" <+> ppPName _X
 ppSig (DRed x cs _As _B) = ppPName x <+> text "Reduction"
 
-ppExp :: Exp -> Box
-ppExp Type = text "Type"
-ppExp (Pi _A (Bind x _B)) = parens (ppName x <+> oft <+> ppExp _A) <+> oft <+> ppExp _B
-ppExp (Lam _A (Bind x a)) = parens (ppName x <+> oft <+> ppExp _A) <+> arr <+> ppExp a
-ppExp (Form _X _Is) = ppPName _X
-ppExp (Con _X as) = ppPName _X
-ppExp (Red _X as) = ppPName _X
-ppExp (Var x) = ppName x
-ppExp (f :@: a) = ppExpParens f <+> ppExpParens a
-
-ppExpParens :: Exp -> Box
-ppExpParens x@Type = ppExp x
-ppExpParens x@(Var _) = ppExp x
-ppExpParens x = parens (ppExp x)
+----------------------------------------------------------------------
 
 ppName :: Name -> Box
 ppName = text . show
@@ -44,3 +64,4 @@ arr = text "->"
 def :: Box
 def = text "="
 
+----------------------------------------------------------------------
