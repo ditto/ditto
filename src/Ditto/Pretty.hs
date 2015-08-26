332789a67b257a59a49defcd858b4d1f8b72737f
larrytheliquid <larrytheliquid@gmail.com>
2015-08-26 13:58:01 -0700

    Group telescopes in pretty printing.

--- src/Ditto/Pretty.hs
+++ src/Ditto/Pretty.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module Ditto.Pretty where
 
 import Ditto.Syntax
@@ -8,14 +9,11 @@ import Text.PrettyPrint.Boxes
 ppExp :: Exp -> Box
 ppExp = ppwExp NoWrap
 
-ppBind :: Name -> Exp -> Box
-ppBind x _A = parens $ ppName x <+> oft <+> ppExp _A
-
 ppwExp :: Wrap -> Exp -> Box
 ppwExp w Type = text "Type"
 ppwExp w (Var x) = ppName x
-ppwExp w (Pi _A (Bind x _B)) = righty w $ ppBind x _A <+> oft <+> ppExp _B
-ppwExp w (Lam _A (Bind x a)) = righty w $ ppBind x _A <+> arr <+> ppExp a
+ppwExp w x@(Pi _ _) = ppPis w x
+ppwExp w x@(Lam _ _) = ppLams w x
 ppwExp w (Form _X _Is) = ppPrim w _X _Is
 ppwExp w (Con x as) = ppPrim w x as
 ppwExp w (Red x as) = ppPrim w x as
@@ -23,7 +21,28 @@ ppwExp w (f :@: a) = lefty w $ ppwExp NoWrapL f <+> ppwExp Wrap a
 
 ppPrim :: Wrap -> PName -> [Exp] -> Box
 ppPrim w x [] = ppPName x
-ppPrim w x as = lefty w $ ppPName x <+> hsep 1 top (map (ppwExp Wrap) as)
+ppPrim w x as = lefty w $ ppPName x <+> hsep1 (map (ppwExp Wrap) as)
+
+----------------------------------------------------------------------
+
+ppPis :: Wrap -> Exp -> Box
+ppPis w (viewPis -> (_As, _B)) = righty w $ hsep1 (map (uncurry ppBind) _As) <+> oft <+> ppExp _B
+
+ppLams :: Wrap -> Exp -> Box
+ppLams w (viewLams -> (as, b)) = righty w $ hsep1 (map (uncurry ppBind) as) <+> arr <+> ppExp b
+
+viewPis :: Exp -> ([(Name, Exp)], Exp)
+viewPis (Pi _A (Bind x _B)) = ((x, _A):_As, _B')
+  where (_As, _B') = viewPis _B
+viewPis a = ([], a)
+
+viewLams :: Exp -> ([(Name, Exp)], Exp)
+viewLams (Lam _A (Bind x b)) = ((x, _A):_As, b')
+  where (_As, b') = viewLams b
+viewLams a = ([], a)
+
+ppBind :: Name -> Exp -> Box
+ppBind x _A = parens $ ppName x <+> oft <+> ppExp _A
 
 ----------------------------------------------------------------------
 
@@ -41,10 +60,20 @@ lefty Wrap = parens
 ----------------------------------------------------------------------
 
 ppSig :: Sigma -> Box
-ppSig (Def x a _A) = (ppName x <+> oft <+> ppExp _A) // (ppName x <+> def <+> ppExp a)
+ppSig (Def x a _A) = ppDefType x _A // ppDefBod x a
 ppSig (DForm _X _Is) = ppPName _X <+> text "Data"
 ppSig (DCon _Y _As _X _Is) = ppPName _Y <+> text "constructor of" <+> ppPName _X
 ppSig (DRed x cs _As _B) = ppPName x <+> text "Reduction"
+
+----------------------------------------------------------------------
+
+ppDefType :: Name -> Exp -> Box
+ppDefType x _A@(Pi _ _) = ppName x <+> ppExp _A
+ppDefType x _A = ppName x <+> oft <+> ppExp _A
+
+ppDefBod :: Name -> Exp -> Box
+ppDefBod x a@(Lam _ _) = ppName x <+> ppExp a
+ppDefBod x a = ppName x <+> def <+> ppExp a
 
 ----------------------------------------------------------------------
 
@@ -63,9 +92,12 @@ oft :: Box
 oft = char ':'
 
 arr :: Box
-arr = text "->"
+arr = text "="
 
 def :: Box
 def = text "="
+
+hsep1 :: [Box] -> Box
+hsep1 = hsep 1 top
 
 ----------------------------------------------------------------------
