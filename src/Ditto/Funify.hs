6b2c2c4dc5aac33bc8047bc7f177e5a2a77c8f3a
larrytheliquid <larrytheliquid@gmail.com>
2015-08-27 15:20:23 -0700

    Only fail unification on clashing constructor heads, not clashing neutral terms.
    Clashing neutral terms technically makes unification get "stuck", but this
    appears to the user as a type error.

--- src/Ditto/Funify.hs
+++ src/Ditto/Funify.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Ditto.Funify where
 import Ditto.Syntax
 import Ditto.Whnf
@@ -12,30 +13,32 @@ import Control.Applicative
 ----------------------------------------------------------------------
 
 funifies :: [Name] -> [Exp] -> [Exp] -> TCM (Maybe Sub)
-funifies xs as1 as2 = (Just <$> funifies' xs as1 as2)
-  `catchError` const (return Nothing)
-
-funifies' :: [Name] -> [Exp] -> [Exp] -> TCM Sub
-funifies' xs [] [] = return []
-funifies' xs (a1:as1) (a2:as2) = do
-  s <- funify xs a1 a2
-  as1' <- subs s as1
-  as2' <- subs s as2
-  -- TODO this may need to be substitution composition
-  (s++) <$> funifies' xs as1' as2'
+funifies xs [] [] = return . Just $ []
+funifies xs (a1:as1) (a2:as2) = funify xs a1 a2 >>= \case
+  Nothing -> return Nothing
+  Just s -> do
+    as1' <- subs s as1
+    as2' <- subs s as2
+    funifies xs as1' as2' >>= \case
+      Nothing -> return Nothing
+      -- TODO this may need to be substitution composition
+      Just s' -> return . Just $ s ++ s'
   where subs s = mapM (flip sub s)
-funifies' _ _ _ = throwError "Unifiying equations of differing lengths"
+funifies _ _ _ = throwError "Unifiying equations of differing lengths"
 
-funify :: [Name] -> Exp -> Exp -> TCM Sub
+funify :: [Name] -> Exp -> Exp -> TCM (Maybe Sub)
 funify xs a1 a2 = do
   a1' <- whnf a1
   a2' <- whnf a2
   funify' xs a1' a2'
 
-funify' :: [Name] -> Exp -> Exp -> TCM Sub
-funify' xs (Var x) a | x `elem` xs && x `notElem` fv a = return [(x, a)]
-funify' xs a (Var x) | x `elem` xs && x `notElem` fv a = return [(x, a)]
-funify' xs (Con x1 as1) (Con x2 as2) | x1 == x2 = funifies' xs as1 as2
-funify' xs a1 a2 = conv a1 a2 >> return []
+funify' :: [Name] -> Exp -> Exp -> TCM (Maybe Sub)
+funify' xs (Var x) a | x `elem` fv a = return Nothing
+funify' xs a (Var x) | x `elem` fv a = return Nothing
+funify' xs (Var x) a | x `elem` xs = return . Just $ [(x, a)]
+funify' xs a (Var x) | x `elem` xs = return . Just $ [(x, a)]
+funify' xs (Con x1 as1) (Con x2 as2) | x1 /= x2 = return Nothing
+funify' xs (Con x1 as1) (Con x2 as2) = funifies xs as1 as2
+funify' xs a1 a2 = conv a1 a2 >> return (Just [])
 
 ----------------------------------------------------------------------
